56cd0cd8f5522534dbc5033d505aacdc6a079cfc
MaddoxDevelopment <jm@madev.me>
2019-06-25 06:28:50 -0500

    test

---
+++ backend/preload.js
@@ -0,0 +1,4 @@
+window.ipcRenderer = require('electron').ipcRenderer;
+window.remote = require('electron').remote;
+window.rspeer = window.rspeer || {};
+window.rspeer.apiUrl = "https://services.rspeer.org/api/";
