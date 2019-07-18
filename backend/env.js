1018aca1c5f035bd283f60111a311d8f0fcb72a0
MaddoxDevelopment <jm@madev.me>
2019-07-17 19:10:42 -0500

    version

--- backend/env.js
+++ backend/env.js
@@ -1,7 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development';
 const isStaging = process.env.NODE_ENV === 'staging';
-const openDevTools = process.env.tools = 'true';
 const isProd = !isDev && !isStaging;
+const openDevTools = process.env.tools = 'true' && !isProd;
 
 module.exports = {
     isDev,
