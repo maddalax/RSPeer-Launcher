c9b160a3ce288235bc736bf6d979ca9b73411f98
MaddoxDevelopment <jm@madev.me>
2019-07-17 19:27:13 -0500

    config fix woops

--- front/src/Config.ts
+++ front/src/Config.ts
@@ -1,4 +1,4 @@
 export const LauncherVersion = '1.12';
-export const isDev = () => window.rspeer.env.isDev;
-export const isStaging = () => window.rspeer.env.isStaging;
-export const isProd = () => window.rspeer.env.isProd;
+export const isDev = () => window.rspeer.env && window.rspeer.env.isDev;
+export const isStaging = () => window.rspeer.env && window.rspeer.env.isStaging;
+export const isProd = () => !window.rspeer.env || window.rspeer.env.isProd;
