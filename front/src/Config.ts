09d40581a43b11fa7bca77267f28f5b39beda1be
MaddoxDevelopment <jm@madev.me>
2019-08-14 07:57:53 -0500

    version

--- front/src/Config.ts
+++ front/src/Config.ts
@@ -1,4 +1,4 @@
-export const LauncherVersion = '1.12';
+export const LauncherVersion = '1.13';
 export const isDev = () => window.rspeer.env && window.rspeer.env.isDev;
 export const isStaging = () => window.rspeer.env && window.rspeer.env.isStaging;
 export const isProd = () => !window.rspeer.env || window.rspeer.env.isProd;
