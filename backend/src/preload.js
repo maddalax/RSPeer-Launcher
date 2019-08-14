932888394eb60dec01cf88b45f4e0a8f556201c8
MaddoxDevelopment <jm@madev.me>
2019-08-14 09:28:38 -0500

    woops

--- backend/src/preload.js
+++ backend/src/preload.js
@@ -2,4 +2,4 @@ window.ipcRenderer = require('electron').ipcRenderer;
 window.remote = require('electron').remote;
 const {isProd, isDev, isStaging} = require('./env');
 window.rspeer = window.rspeer || {env : {isProd, isStaging, isDev}};
-window.rspeer.apiUrl = "http://localhost/api/";
+window.rspeer.apiUrl = "https://services.rspeer.org/api/";
